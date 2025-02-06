aa20e5029912e1e90aface938cc50ecbc558408d
sosmani3 <sosmani3@depaul.edu>
2025-02-06 16:31:17 -0600

    Fix routing and remove unused ContactForm component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Import Routes
 import AboutMe from './components/AboutMe';
 import MyProjects from './components/MyProjects';
 import ContactForm from './components/ContactForm';
@@ -9,14 +9,12 @@ const App = () => {
   return (
     <Router>
       <div className="app">
-        <Switch>
-          <Route path="/about" component={AboutMe} />
-          <Route path="/projects" component={MyProjects} />
-          <Route path="/contact" component={ContactForm} />
-          <Route path="/" exact>
-            <h1>Welcome to My Portfolio</h1>
-          </Route>
-        </Switch>
+        <Routes> {/* Use Routes instead of Switch */}
+          <Route path="/about" element={<AboutMe />} /> {/* Use element instead of component */}
+          <Route path="/projects" element={<MyProjects />} /> {/* Use element instead of component */}
+          <Route path="/contact" element={<ContactForm />} /> {/* Use element instead of component */}
+          <Route path="/" exact element={<h1>Welcome to My Portfolio</h1>} /> {/* Use element instead of component */}
+        </Routes>
       </div>
     </Router>
   );
